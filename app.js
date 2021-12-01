daaaf963da4a9926ab3fa3ed1f49a0c91873fa9e
Dee <dgrowette@gmail.com>
2021-12-01 12:13:24 -0800

    added inquire and npm. ran name test as well

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template')
+const inquirer = require('inquirer')
 
-// This holds the user command-line arguments.
-const profileDataArgs = process.argv.slice(2);
 
-console.log(profileDataArgs);
-
-const [name, github] = profileDataArgs;
-
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Protfolio complete! Check out index.html to see the output!');
-})
+inquirer
+   .prompt([
+       {
+           type: 'input',
+           name: 'name',
+           message: 'What is your name?'
+       }
+   ]) 
+   .then(answers => console.log(answers))
 
 
 
@@ -38,22 +31,13 @@ fs.writeFile('index.html', generatePage(name, github), err => {
 
 
 
+// const fs = require('fs')
+// const generatePage = require('./src/page-template')
 
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
 
-
-
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
+//     console.log('Protfolio complete! Check out index.html to see the output!');
+// })
